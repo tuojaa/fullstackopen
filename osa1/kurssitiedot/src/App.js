d9761c463db68e99570584207dc6fe7449c23225
Tuomas Jaanu <tuomas.jaanu@gmail.com>
2021-03-27 18:46:21 +0200

    Harjoitukset 1.1-1.5

---
+++ osa1/kurssitiedot/src/App.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      {props.part.name} {props.part.exercises}
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  
+  const sum = props.parts.reduce((a, b) => {
+    return a + b.exercises;
+  }, 0)
+
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App
