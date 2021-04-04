98977db27fecca955de68c8871ecbf7071054553
Tuomas Jaanu <tuomas.jaanu@gmail.com>
2021-04-04 21:18:17 +0300

    Tehtävä 7.5

--- osa7/country-hook/src/App.js
+++ osa7/country-hook/src/App.js
@@ -18,7 +18,16 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {    
+      axios
+        .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+        .then(response => {        
+          setCountry({ data: response.data[0], found: true })
+        })
+        .catch(error => {
+          setCountry({ found: false })
+        })
+  }, [name])
 
   return country
 }
