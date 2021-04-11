cfdb1cf878671b870239e1820c7361823b8535f5
Tuomas Jaanu <tuomas.jaanu@gmail.com>
2021-04-11 10:11:34 +0300

    Exercise 9.7

--- osa9/exercise/bmiCalculator.ts
+++ osa9/exercise/bmiCalculator.ts
@@ -1,25 +1,4 @@
-function calculateBmi({ height, weight }: { height: number; weight: number }): string {
-  if ((!height) || (!weight)) {
-    throw new Error('Must provide height and weight as numbers');
-  }
-  const bmi = weight / ((height / 100) ** 2);
-  if (bmi < 15)
-    return 'Very severely underweight';
-  if (bmi < 16)
-    return 'Severely underweight';
-  if (bmi < 18.5)
-    return 'Underweight';
-  if (bmi < 25)
-    return 'Normal (healthy weight)';
-  if (bmi < 30)
-    return 'Overweight';
-  if (bmi < 35)
-    return 'Obese Class I (Moderately obese)';
-  if (bmi < 40)
-    return 'Obese Class II (Severely obese)';
-  return 'Obese Class III (Very severely obese)';
-}
-export default calculateBmi;
+import calculateBmi from './services/bmi';
 
 try {
   if (process.argv.length !== 4) {
--- osa9/exercise/index.ts
+++ osa9/exercise/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import calculateBmi from './bmiCalculator';
+import calculateBmi from './services/bmi';
+import calculateExercises, { ExerciseResult } from './services/exercise';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -14,6 +16,26 @@ app.get('/bmi', (req, res) => {
     
     const bmi = calculateBmi({ height, weight });
     res.json({ bmi, height, weight });
+  } catch(error) {
+    if(error instanceof Error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      throw error;
+    }
+  }
+});
+
+interface ExerciseBody {
+  target: number,
+  daily_exercises: Array<number>
+}
+
+app.post('/exercises', (req, res) => {
+  try {
+    console.log(req.body);
+    const query : ExerciseBody = req.body as ExerciseBody;
+    const result : ExerciseResult = calculateExercises(query.daily_exercises, query.target);
+    res.json(result);
   } catch(error) {
     if(error instanceof Error) {
       res.status(400).json({ error: error.message });
